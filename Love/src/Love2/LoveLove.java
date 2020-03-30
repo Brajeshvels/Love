57f796fed3b8e4ce0431ef363130e307adb483a7
Brajeshvels <brajesh.yes@gmail.com>
2020-03-31 02:35:05 +0530

    some modification done

--- Love/src/Love2/LoveLove.java
+++ Love/src/Love2/LoveLove.java
@@ -6,7 +6,7 @@ public class LoveLove {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("kjkjkkjkkldkjjkjkjkjljlkjnmmlk");
+		System.out.println("kjkjkkjkkldkjjkkkjkjkjljlkjnmmlk");
 		
 	}
 	
